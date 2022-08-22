9342f515220807f7f2ac992d7fa61526ab6b1d4e
Nilesh <nkpnilesh13@gmail.com>
2022-08-22 16:52:10 +0530

    business-partners-page

--- src/components/AgencyResources/AgencyResources.js
+++ src/components/AgencyResources/AgencyResources.js
@@ -1,9 +1,141 @@
-const AgencyResources = () =>{
-    return(
-        <h1>
-           Agency Resources Page 
-        </h1>
+import { useEffect, useState } from "react"
+import "./agencyresources.css"
+import { useNavigate } from "react-router-dom";
+import classNames from "classnames";
+
+// Images Import
+import agency_resources_hero_image1 from "../../images/agency-resources/agency-resources-hero/agency-resources-hero-image1.png"
+import agency_resources_hero_image2 from "../../images/agency-resources/agency-resources-hero/agency-resources-hero-image2.png"
+import agency_resources_hero_image3 from "../../images/agency-resources/agency-resources-hero/agency-resources-hero-image3.gif"
+import agency_resources_hero_image4 from "../../images/agency-resources/agency-resources-hero/agency-resources-hero-image4.png"
+
+// Next UI Imports
+import { Collapse, Text } from "@nextui-org/react";
+// import { AiOutlinePlus } from "react-icons/ai"
+import { BsPlusLg } from "react-icons/bs";
+import { BiPlus } from "react-icons/bi"
+
+const HowPinterestWorks = () => {
+    const navigate = useNavigate();
+    const path = window.location.pathname;
+
+    const [combinationNumber, setCombinationNumber] = useState("combinationOne")
+
+    const changeCombination = () => {
+        console.log(window.scrollY)
+        if (window.scrollY >= 0 && window.scrollY <= 956) {
+            setCombinationNumber("combinationOne")
+        }
+        else if (window.scrollY >= 956 && window.scrollY <= 3688) {
+            setCombinationNumber("combinationTwo")
+        }
+        else {
+            setCombinationNumber("combinationThree")
+        }
+    }
+
+    useEffect(() => {
+        changeCombination()
+        window.addEventListener("scroll", changeCombination)
+    })
+
+    return (
+        <div className={`ar_${combinationNumber}`}>
+            <div className={classNames(`ar_navbar_${combinationNumber}`, "ar_navbar")}>
+                <div className="ar_navbar_logo">Business</div>
+                <div className="ar_navbar_items" >
+                    <div class="ar_navbar_dropdown">
+                        <p className={classNames("ar_navbar_dropbtn", `${path === "/how-pinterest-works" ? "navbar_active" : ""}`)}>Why pinterest?</p>
+                        <div className={classNames(`ar_navbar_dropdown_content_${combinationNumber}`, "ar_navbar_dropdown_content")}>
+                            <p onClick={() => navigate("/how-pinterest-works")} className={path === "/how-pinterest-works" ? "navbar_active" : ""}>How pinterest works</p>
+                            <p onClick={() => navigate("/getting-started")}>Getting started</p>
+                            <p onClick={() => navigate("/pinterest-audience")}>The Pinterest audience</p>
+                            <p onClick={() => navigate("/success-stories")}>Success story</p>
+                        </div>
+                    </div>
+                    <div class="ar_navbar_dropdown">
+                        <p className="ar_navbar_dropbtn">Create content</p>
+                        <div className={classNames(`ar_navbar_dropdown_content_${combinationNumber}`, "ar_navbar_dropdown_content")}>
+                            <p onClick={() => navigate("/creators")}>Pinterest for creators</p>
+                            <p onClick={() => navigate("/how-to-make-pins")}>Making pins: Tips and Tricks</p>
+                            <p onClick={() => navigate("/creative-best-practices")}>Best practices</p>
+
+                        </div>
+                    </div>
+                    <div class="ar_navbar_dropdown">
+                        <p className="ar_navbar_dropbtn">Insights</p>
+                        <div className={classNames(`ar_navbar_dropdown_content_${combinationNumber}`, "ar_navbar_dropdown_content")}>
+                            <p onClick={() => navigate("/pinterest-predicts")}>Pinterest predicts</p>
+                        </div>
+                    </div>
+                    <div class="ar_navbar_dropdown">
+                        <p className="ar_navbar_dropbtn">Resources</p>
+                        <div className={classNames(`ar_navbar_dropdown_content_${combinationNumber}`, "ar_navbar_dropdown_content")}>
+                            <p onClick={() => navigate("/pinterest-business-partners")}>Pinterest Business Partners</p>
+                            <p onClick={() => navigate("/agency-resources")}>Agency Resources</p>
+                            <p onClick={() => navigate("/guides-and-education")}>Guides and Education</p>
+                        </div>
+                    </div>
+                    <div className={classNames(`ar_navbar_login_${combinationNumber}`, "ar_navbar_login")} onClick={() => navigate("/login")}>
+                        Log in
+                    </div>
+                    <div className={classNames(`ar_navbar_signup_${combinationNumber}`, "ar_navbar_signup")} onClick={() => navigate("/create")}>
+                        Sign up
+                    </div>
+                </div>
+            </div>
+            <div className="ar_hero">
+                <div className="ar_hero_images">
+                    <div className="ar_hero_images_leftcol">
+                        <img src={agency_resources_hero_image1} width="289px" height="346px" style={{ objectFit: "cover", borderRadius: "2rem", padding: "0.6rem" }} />
+                        <img src={agency_resources_hero_image2} width="289px" height="346px" style={{ objectFit: "cover", borderRadius: "2rem", padding: "0.6rem" }} />
+                    </div>
+                    <div className="ar_hero_images_rightcol">
+                        <img src={agency_resources_hero_image3} width="289px" height="385px" style={{ objectFit: "cover", borderRadius: "2rem", padding: "0.6rem" }} />
+                        <img src={agency_resources_hero_image4} width="289px" height="346px" style={{ objectFit: "cover", borderRadius: "2rem", padding: "0.6rem" }} />
+                    </div>
+                </div>
+                <div className="ar_hero_content">
+                    <h1>Help your clients</h1>
+                    <h1>thrive on Pinterest</h1>
+                    <p>We’re here to help you and your clients succeed. From your</p>
+                    <p>first Pinterest pitch to measuring results, our agency</p>
+                    <p>resources support you every step of the way.</p>
+                </div>
+            </div>
+            <div className="ar_pitch">
+                <div className="ar_pitch_left">
+                    <div className="ar_pitch_left_content">
+                    <h1>Why pitch</h1>
+                    <h1>Pinterest</h1>
+                    <div className="ar_pitch_left_para">
+                        <p>Pinterest is a full-funnel solution that reaches high-intent</p>
+                        <p>audiences. It’s the only platform where people actively plan for</p>
+                        <p>what’s next, so you can reach them at critical moments</p>
+                        <p>throughout the decision-making process.</p>
+                    </div>
+                    <div className="ar_pitch_left_button" onClick={() => navigate("/pinterest-audience")}>
+                        Read success stories
+                    </div>
+                    </div>
+                   
+                </div>
+                <div className="ar_pitch_right">
+                    <div className="ar_pitch_right_image"></div>
+                    <div className={classNames(`ar_pitch_right_card_${combinationNumber}`, "ar_pitch_right_card")}>
+                        <h1>61%</h1>
+                        <p>of Pinners in the UK have</p>
+                        <p>gone on to make a</p>
+                        <p>purchase as a result of</p>
+                        <p>seeing branded content.<sup>1</sup></p>
+                    </div>
+                </div>
+            </div>
+            <div className="ar_agency">
+                <h1>Afency</h1>
+            </div>
+        </div>
     )
 }
 
-export default AgencyResources;
+export default HowPinterestWorks;
--- src/components/BusinessPartners/BusinessPartners.js
+++ src/components/BusinessPartners/BusinessPartners.js
@@ -1,8 +1,245 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import "./businesspartners.css";
+import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
+
 const BusinessPartners = () =>{
+    const navigate = useNavigate();
+    const path = window.location.pathname;
+
+    const [combinationNumber, setCombinationNumber] = useState("combinationOne");
+
+    const changeCombination = () => {
+        console.log(window.scrollY);
+        if (window.scrollY >= 0 && window.scrollY <= 430) {
+            setCombinationNumber("combinationOne");
+        }
+        else if(window.scrollY >= 430 && window.scrollY <= 1260){
+            setCombinationNumber("combinationTwo");
+        }
+        else {
+            setCombinationNumber("combinationThree");
+        }
+    };
+
+    useEffect(() => {
+        changeCombination();
+        window.addEventListener("scroll", changeCombination);
+    });
+
     return(
-        <h1>
-            Business Partners Page
-        </h1>
+        <div className={`pbp_${combinationNumber}`}>
+            <div
+                className={classNames(`pbp_navbar_${combinationNumber}`, "pbp_navbar")}
+            >
+                <div className="pbp_navbar_logo">Business</div>
+                <div className="pbp_navbar_items">
+                    <div class="pbp_navbar_dropdown">
+                        <p className={classNames("pbp_navbar_dropbtn")}>Why pinterest?</p>
+                        <div
+                            className={classNames(
+                                `pbp_navbar_dropdown_content_${combinationNumber}`,
+                                "pbp_navbar_dropdown_content"
+                            )}
+                        >
+                            <p onClick={() => navigate("/how-pinterest-works")}>
+                                How pinterest works
+                            </p>
+                            <p onClick={() => navigate("/getting-started")}>
+                                Getting started
+                            </p>
+                            <p onClick={() => navigate("/pinterest-audience")}>
+                                The Pinterest audience
+                            </p>
+                            <p onClick={() => navigate("/success-stories")}>Success story</p>
+                        </div>
+                    </div>
+                    <div class="pbp_navbar_dropdown">
+                        <p className="pbp_navbar_dropbtn">Create content</p>
+                        <div
+                            className={classNames(
+                                `pbp_navbar_dropdown_content_${combinationNumber}`,
+                                "pbp_navbar_dropdown_content"
+                            )}
+                        >
+                            <p onClick={() => navigate("/creators")}>
+                                Pinterest for creators
+                            </p>
+                            <p onClick={() => navigate("/how-to-make-pins")}>
+                                Making pins: Tips and Tricks
+                            </p>
+                            <p onClick={() => navigate("/creative-best-practices")}>
+                                Best practices
+                            </p>
+                        </div>
+                    </div>
+                    <div class="pbp_navbar_dropdown">
+                        <p className="pbp_navbar_dropbtn">Insights</p>
+                        <div
+                            className={classNames(
+                                `pbp_navbar_dropdown_content_${combinationNumber}`,
+                                "pbp_navbar_dropdown_content"
+                            )}
+                        >
+                            <p onClick={() => navigate("/pinterest-predicts")}>
+                                Pinterest predicts
+                            </p>
+                        </div>
+                    </div>
+                    <div class="pbp_navbar_dropdown">
+                        <p className="pbp_navbar_dropbtn">Resources</p>
+                        <div
+                            className={classNames(
+                                `pbp_navbar_dropdown_content_${combinationNumber}`,
+                                "pbp_navbar_dropdown_content"
+                            )}
+                        >
+                            <p onClick={() => navigate("/pinterest-business-partners")}>
+                                Pinterest Business Partners
+                            </p>
+                            <p onClick={() => navigate("/agency-resources")}>
+                                Agency Resources
+                            </p>
+                            <p onClick={() => navigate("/guides-and-education")}>
+                                Guides and Education
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        className={classNames(
+                            `pbp_navbar_login_${combinationNumber}`,
+                            "pbp_navbar_login"
+                        )}
+                        onClick={() => navigate("/login")}
+                    >
+                        Log in
+                    </div>
+                    <div
+                        className={classNames(
+                            `pbp_navbar_signup_${combinationNumber}`,
+                            "pbp_navbar_signup"
+                        )}
+                        onClick={() => navigate("/create")}
+                    >
+                        Sign up
+                    </div>
+                </div>
+            </div>
+            <div className="pbp_hero">
+                <h1>Partner up for better results</h1>
+                <p>Pinterest Business Partners make it easier for you to create great creative, scale your</p>
+                <p>ads and measure performance. We’ve curated a great list of partners that know all</p>
+                <p>about Pinterest—and they’re ready to help you grow.</p>
+                <h2>Interested in becoming a partner? <span>Apply now.</span></h2>
+            </div>
+            <div className="pbp_range">
+                <h1>Range of Partnership Specialties</h1>
+                <h3>Pinterest Business Partners span a range of technical and creative specialties. Depending on your goals, you</h3>
+                <h3>might want to work with multiple partners at once.</h3>
+                <div className="pbp_range_content">
+                    <div className="pbp_range_contentbox">
+                        <h2>
+                        Advertising
+                        </h2>
+                        <p>Work with experts in Pinterest media strategy,</p>
+                        <p>media buying and campaign management.</p>
+                    </div>
+                    <div className="pbp_range_contentbox">
+                        <h2>
+                        Audiences
+                        </h2>
+                        <p>Get help with onboarding first-party data so</p>
+                        <p>that you can use advanced targeting features.</p>
+                    </div>
+                    <div className="pbp_range_contentbox">
+                        <h2>
+                        Content
+                        </h2>
+                        <p>Build a stronger Pinterest presence with tools</p>
+                        <p>such as Pin schedulers and engagement</p>
+                        <p>dashboards.</p>
+                    </div>
+                    <div className="pbp_range_contentbox">
+                        <h2>
+                        Creative
+                        </h2>
+                        <p>Scale your success with creative tools or work</p>
+                        <p>with an approved Pinterest design expert.</p>
+                    </div>
+                    <div className="pbp_range_contentbox">
+                        <h2>
+                        Measurement
+                        </h2>
+                        <p>Dig deeper into campaign performance with</p>
+                        <p>sophisticated solutions and custom reports.</p>
+                    </div>
+                    <div className="pbp_range_contentbox">
+                        <h2>
+                        Shopping
+                        </h2>
+                        <p>Make it easier for people to shop for your</p>
+                        <p>products with tools such as feed managers and</p>
+                        <p>custom experiences.</p>
+                    </div>
+                </div>
+            </div>
+            <div className = "pbp_filter">
+                <p>Filter by</p>
+                <div className = "pbp_filter_options">
+                    <h2>Speciality</h2>
+                    <h2>Service model</h2>
+                    <h2>Pricing model</h2>
+                </div>
+                <p>We don't have any results for those filters. Please try again.</p>
+            </div>
+            <div className="pbp_footer">
+                <div className="pbp_footer_bottom">
+                    <div className="pbp_footer_bottom_left">
+                        <h1>Pinterest</h1>
+                        <select name="language" id="language">
+
+                            <option value="English(India)">English(India)</option>
+                            <option value="English(US)">English(US)</option>
+                            <option value="Bahasa Indonesia">Bahasa Indonesia</option>
+                            <option value="Bahasa Malaysia">Bahasa Malaysia</option>
+
+                        </select>
+                        <span>©️ 2022 Pinterest</span>
+                    </div>
+                    <div className="pbp_footer_bottom_right">
+                        <div className="pbp_footer_bottom_right_column">
+                            <h4>About</h4>
+                            <ul>
+                                <li>Our Pinterest Profile</li>
+                                <li>Newsroom</li>
+                                <li>Career</li>
+                                <li>Brand guidelines</li>
+                            </ul>
+                        </div>
+                        <div className="pbp_footer_bottom_right_column">
+                            <h4>Resources</h4>
+                            <ul>
+                                <li>Help Center</li>
+                                <li>Business Community</li>
+                                <li>For developers</li>
+                                <li>For investors</li>
+                            </ul>
+                        </div>
+                        <div className="pbp_footer_bottom_right_column">
+                            <h4>Policies</h4>
+                            <ul>
+                                <li>Copyright & Trademark</li>
+                                <li>Terms of service</li>
+                                <li>Privacy and Cookies</li>
+                                <li>Personalised ads</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
